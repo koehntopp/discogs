19fbd765b6d483a18de83993e297191f3b4b73a3
Frank Köhntopp <frank@koehntopp.com>
2026-02-01 09:03:12 +0100

    uv updates, change order for lyrics

--- nzbfix.py
+++ nzbfix.py
@@ -1,3 +1,16 @@
+# /// script
+# dependencies = [
+#   "rich",
+#   "tqdm",
+#   "pytaglib",
+#   "requests",
+#   "discogs_client",
+#   "alive-progress",
+#   "pyacoustid",
+#   "drmeter"
+# ]
+# ///
+
 import os, sys
 from shlex import quote
 from subprocess import call, DEVNULL
@@ -21,8 +34,8 @@ call('dot_clean ' + quote(flacdir), shell=True, stdout=DEVNULL, stderr=DEVNULL)
 call('python3 /Users/koehntopp/src/discogs/calculate_dr.py ' + quote(flacdir), shell=True)
 
 timelog('Starting replay gain calculation in', flacdir)
-call('/Applications/rsgain easy --skip-existing ' + quote(flacdir), shell=True, stdout=DEVNULL, stderr=DEVNULL)
+call('/Applications/rsgain easy -m MAX --skip-existing ' + quote(flacdir), shell=True, stdout=DEVNULL, stderr=DEVNULL)
 
 call('python3 /Users/koehntopp/src/discogs/calculate_fp.py ' + quote(flacdir), shell=True)
-call('python3 /Users/koehntopp/src/discogs/update_lyrics.py ' + quote(flacdir), shell=True)
 call('python3 /Users/koehntopp/src/discogs/fixtags.py ' + quote(flacdir), shell=True)
+call('python3 /Users/koehntopp/src/discogs/update_lyrics.py ' + quote(flacdir), shell=True)
